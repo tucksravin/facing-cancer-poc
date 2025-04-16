fd40c5137d30332e924eb5c32d8ff6bc6c84bb3d
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2025-04-16 14:32:58 -0700

    removed prismic stuff

--- .svelte-kit/generated/client/app.js
+++ .svelte-kit/generated/client/app.js
@@ -5,17 +5,15 @@ export const nodes = [
 	() => import('./nodes/1'),
 	() => import('./nodes/2'),
 	() => import('./nodes/3'),
-	() => import('./nodes/4'),
-	() => import('./nodes/5')
+	() => import('./nodes/4')
 ];
 
 export const server_loads = [0];
 
 export const dictionary = {
-		"/confirm": [4],
-		"/hi": [5],
-		"/[[preview=preview]]": [2],
-		"/[[preview=preview]]/[uid]": [~3]
+		"/confirm": [3],
+		"/hi": [4],
+		"/[[preview=preview]]": [2]
 	};
 
 export const hooks = {
--- .svelte-kit/types/src/routes/$types.d.ts
+++ .svelte-kit/types/src/routes/$types.d.ts
@@ -11,8 +11,8 @@ type OutputDataShape<T> = MaybeWithVoid<Omit<App.PageData, RequiredKeys<T>> & Pa
 type EnsureDefined<T> = T extends null | undefined ? {} : T;
 type OptionalUnion<U extends Record<string, any>, A extends keyof U = U extends U ? keyof U : never> = U extends unknown ? { [P in Exclude<A, keyof U>]?: never } & U : never;
 export type Snapshot<T = any> = Kit.Snapshot<T>;
-type LayoutRouteId = RouteId | "/[[preview=preview]]" | "/[[preview=preview]]/[uid]" | "/confirm" | "/hi" | null
-type LayoutParams = RouteParams & { preview?: MatcherParam<typeof import('../../../../src/params/preview.js').match>; uid?: string }
+type LayoutRouteId = RouteId | "/[[preview=preview]]" | "/confirm" | "/hi" | null
+type LayoutParams = RouteParams & { preview?: MatcherParam<typeof import('../../../../src/params/preview.js').match> }
 type LayoutServerParentData = EnsureDefined<{}>;
 type LayoutParentData = EnsureDefined<{}>;
 
