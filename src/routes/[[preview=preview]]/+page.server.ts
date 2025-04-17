fb0105fe2134dae4ada39ea21bfd65ede9def952
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2025-04-17 14:44:37 -0700

    set params

--- src/routes/[[preview=preview]]/+page.server.ts
+++ src/routes/[[preview=preview]]/+page.server.ts
@@ -1,5 +1,14 @@
 import { redirect } from '@sveltejs/kit';
+import { page } from '$app/state';
+import { cancerSubtype } from '$lib/stores/cancerSubtype';
+import { cancerType } from '$lib/stores/cancerType';
 
 export function load() {
+	if (page.url.searchParams.has('type')) {
+		cancerType.set(page.url.searchParams.get('type') || '');
+	  }
+	  if (page.url.searchParams.has('subtype')) {
+		cancerSubtype.set(page.url.searchParams.get('subtype') || '');
+	  }
 	redirect(308, '/hi');
 }
